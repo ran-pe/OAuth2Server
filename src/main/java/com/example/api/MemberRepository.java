5077218cf569664d2f0b6334e36e012557dba37a
youngran.kwon <youngran.kwon@lemonhc.com>
2022-02-14 09:25:24 +0900

    2022.02.14 commit

--- src/main/java/com/example/api/MemberRepository.java
+++ src/main/java/com/example/api/MemberRepository.java
@@ -3,5 +3,5 @@ package com.example.api;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "member")
+@RepositoryRestResource
 public interface MemberRepository extends PagingAndSortingRepository<Member, Long> { }
